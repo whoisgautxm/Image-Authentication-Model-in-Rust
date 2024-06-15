fcbabe44fd6fadcf283ffd7768407530122ca9c4
Shivannsh <bb1221392@iitd.ac.in>
2024-06-15 14:40:48 +0530

    Image Regeneration and Decryption (Iteration-1) fix

--- src/image_verification.rs
+++ src/image_verification.rs
@@ -1,7 +1,8 @@
 
 use crate::merkle_tree::{MerkleTree, ri_array};
 
-pub fn image_verification(fake_merkle_tree: MerkleTree, original_merkle_tree: MerkleTree) {
+pub fn image_verification(fake_merkle_tree: MerkleTree, original_merkle_tree: MerkleTree) -> Vec<u32>{
     let ri = ri_array(&original_merkle_tree, &fake_merkle_tree);
     println!("Tampered result array: {:?} {:?}", ri,ri.len());
+    ri
 }
